e1fd032b7e23beea982544a8f4634d8ec793ea46
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-03-07 05:19:11 +0300

    updated

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-
+window.onload = () => {
+    const container = document.getElementById("container");
+    const createLabel = (name) => {
+      const label = document.createElement("div");
+      label.className = "label";
+      label.textContent = name;
+      container.appendChild(label);
+    }
+    const letters = charRange("A", "J");
+    letters.forEach(createLabel);
+    range(1, 99).forEach(number => {
+      createLabel(number);
+      letters.forEach(letter => {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.id = letter + number;
+        input.ariaLabel = letter + number;
+        container.appendChild(input);
+      })
+    })
+  }
